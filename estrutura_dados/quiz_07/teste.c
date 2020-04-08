400d74ad2028de1fe2483de25de724343e10fbf8
Robson Melchiades <robson_melchiades@hotmail.com>
2020-04-08 20:12:38 -0300

    teste aula

--- estrutura_dados/quiz_07/teste.c
+++ estrutura_dados/quiz_07/teste.c
@@ -5,6 +5,6 @@ int main(){
     int teste[] = {1,2,3,4,5,6};
     teste++;
     for(int i=0; i<sizeof(teste)/sizeof(int);i++){
-        printf("%d\n", teste[i]);
+        printf("%d\nsdsdsd", teste[i]);
     }
 }
